564d2ed5d342efad4aef32d74feb79a1e72e486f
LCaudillo <luiscaudillop@gmail.com>
2026-01-28 17:58:26 -0600

    Fixed error that make the app of analytics and its template crash

--- src/analytics/views.py
+++ src/analytics/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import TemplateView, View
 from django.shortcuts import render
 from typing import Any
+from django.db.models import Sum
+from django.utils import timezone
 from django.http.request import HttpRequest as HttpRequest
+from django.db.models.functions import TruncDay
+
+from datetime import timedelta
 
 from order.models import Order
 
@@ -25,4 +30,37 @@ class SalesView(LoginRequiredMixin, TemplateView):
         context["paid_orders"]= qs.recent().not_refunded().by_status(status="paid")[:5]
         print(context)
         return context
+
+class SalesAjaxView(View):
+    def get(self, request, *args, **kwargs):
+        sales_type = request.GET.get("type", "week")
+        today = timezone.now()
+
+        if sales_type == "week":
+            start_date = today - timedelta(days=7)
+        elif sales_type == "month":
+            start_date = today - timedelta(days=30)
+        else:
+            start_date = today - timedelta(days=7)
+
+        qs = (
+            Order.objects
+            .filter(updated__gte=start_date, status="paid")
+            .annotate(day=TruncDay("updated"))
+            .values("day")
+            .annotate(total=Sum("total"))
+            .order_by("day")
+        )
+
+        labels = []
+        data = []
+
+        for item in qs:
+            labels.append(item["day"].strftime("%d %b"))
+            data.append(item["total"])
+
+        return JsonResponse({
+            "labels": labels,
+            "data": data
+        })
 
